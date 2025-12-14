bfcd29d714628a78842e0bebeeb63bb3b5080717
SandiZulian <zuliansyahsandi4@gmail.com>
2025-12-14 21:12:40 +0700

    Delete Penilaian_Mahasiswa directory

--- Penilaian_Mahasiswa/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.penilaian_mahasiswa {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires java.base;
-     
-    opens com.mycompany.penilaian_mahasiswa to javafx.fxml;
-    exports com.mycompany.penilaian_mahasiswa;
-}
